66b09cb8880a4a93b84fbc0a3654d65e9b4d5fec
Mohamad Ilham Heryanto <ilhama36@gmail.com>
2023-06-01 19:38:01 +0700

    add Readme, remove unimportant file

--- ios/Pods/Target Support Files/Pods-SurplusApp-SurplusAppTests/Pods-SurplusApp-SurplusAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SurplusApp_SurplusAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_SurplusApp_SurplusAppTests
-@end
--- ios/Pods/Target Support Files/Pods-SurplusApp/Pods-SurplusApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SurplusApp : NSObject
-@end
-@implementation PodsDummy_Pods_SurplusApp
-@end
