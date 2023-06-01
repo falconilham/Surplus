f685a39d6702278efadaa9eeb9803fe2d808c86f
Mohamad Ilham Heryanto <ilhama36@gmail.com>
2023-06-01 16:37:20 +0700

    add debounce, add search query and infinite scroll

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,28 @@
 import axios from 'axios';
+import store from '../redux';
 
 const api = axios.create({
   baseURL: 'https://dummyjson.com/',
 });
+
+api.interceptors.request.use(
+  config => {
+    const state = store.getState();
+    const token = state.auth.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    config.headers['Content-Type'] = 'application/json';
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  response => response,
+  error => Promise.reject(error),
+);
 
 export default api;
